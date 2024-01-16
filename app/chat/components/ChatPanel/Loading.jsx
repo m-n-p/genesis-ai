6d5540e45f783764b376876d36a5b67949ea5b2f
nileshaggarwal <aggarwalnilesh6@gmail.com>
2024-01-17 00:43:18 +0530

    added thoughts to loading screen

--- app/chat/components/ChatPanel/Loading.jsx
+++ app/chat/components/ChatPanel/Loading.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Thoughts } from "../../../static/thoughts";
+const Loading = ({ loading = true }) => {
+  const [randomNumber, setRandomNumber] = useState(0);
 
-const Loading = () => {
+  useEffect(() => {
+    const updateRandomNumber = async () => {
+      try {
+        let number = Math.floor(Math.random() * 40);
+        await setRandomNumber(number);
+      } catch (error) {
+        console.error("Error updating random number:", error);
+      }
+    };
+
+    if (loading) {
+      updateRandomNumber();
+    }
+
+    const intervalId = setInterval(updateRandomNumber, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  console.log(Thoughts);
+
   return (
     <div className={"w-full flex items-center bg-[#D9D9D9] bg-opacity-10 "}>
-      <div className="flex space-x-3 py-4 w-4/5 mx-auto flex-col items-center">
+      <div className="flex space-x-3 py-4 w-4/5 mx-auto flex-col items-center space-y-2">
         <p>Our knowledge is working at your service...</p>
-
+        <h2 className="font-semibold text-lg">
+          {Thoughts[randomNumber].title}
+        </h2>
+        <p className="text-sm font-light italic">
+          {`"${Thoughts[randomNumber].description}"`}
+        </p>
+        <a
+          className="text-sm"
+          href={Thoughts[randomNumber].link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          View in detail
+        </a>
         {/* <p className="max-w-full text-wrap	 flex items-center ">{text}</p> */}
       </div>
     </div>
---
+++ app/static/thoughts.js
@@ -0,0 +1,122 @@
+export const Thoughts = [
+  {
+    title: "Sky's the Limit with Hyundai's eVTOL S-A2",
+    description:
+      "Hyundai-backed Supernal unveils S-A2 eVTOL aircraft, boasting 120mph speed, 40-mile range, and unprecedented noise reduction. A major step in urban air mobility, set to commence operations in 2028.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-urban-air-mobility-supernal-s-s-a2-unveiling",
+  },
+  {
+    title: "Agroshift's Agritech Breakthrough in Bangladesh",
+    description:
+      "Agroshift, a Bangladesh agritech startup, disrupts agri-commerce with its largest pre-seed funding and a unique 'phy-gital' platform, serving over 170,000 users and growing revenues by 350%.",
+    link: "https://www.marketunwinded.com/market-story/empowering-bangladesh-s-agricultural-landscape-agroshift-s-trailblazing-journey",
+  },
+  {
+    title: "Mental Wellness Startup Amaha Health Secures $6M",
+    description:
+      "Amaha Health, previously InnerHour, raises $6M to expand its tech-driven mental wellness services in India, signaling a surge in demand for accessible mental health care solutions.",
+    link: "https://www.marketunwinded.com/market-story/amaha-health-s-funding-boost-and-market-position",
+  },
+  {
+    title: " Texas A&M's $5.2M Grant for Onion Harvest Innovation",
+    description:
+      "Texas A&M AgriLife receives over $5M USDA grant to transform short-day onion production with new harvesting technology, promising a sustainable future in agriculture.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-short-day-onion-harvesting-texas-a-m-agrilife-s-groundbreaking-project",
+  },
+  {
+    title: "Kia and Uber Join Forces for Eco-Friendly Mobility",
+    description:
+      "Kia and Uber's partnership at CES revolutionizes ride-hailing with Purpose-Built Vehicles, promising zero-emission and personalized mobility experiences.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-ride-hailing-kia-and-uber-s-strategic-leap-into-personalized-mobility",
+  },
+  {
+    title: "Infosys Makes Strategic Semiconductor Play with InSemi",
+    description:
+      "Infosys acquires InSemi to boost its semiconductor capabilities, marking a strategic leap in AI, 5G, and smart devices, showcasing its commitment to tech-driven solutions.",
+    link: "https://www.marketunwinded.com/market-story/infosys-acquires-insemi-a-strategic-leap-into-semiconductor-design",
+  },
+  {
+    title: "GKN Aerospace's $63M Additive Manufacturing Investment",
+    description:
+      "GKN Aerospace invests $63M in Swedish facility for additive manufacturing, reducing material waste by 80% and revolutionizing aircraft engine production.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-aerospace-manufacturing-gkn-aerospace-s-additive-manufacturing-leap",
+  },
+  {
+    title: "Northvolt's EV Battery Plant: EU's €902M Support",
+    description:
+      "Northvolt secures EU's €902M backing for a new EV battery plant in Germany, a strategic move supporting the EU's green transition and enhancing Europe's position in the EV market.",
+    link: "https://www.marketunwinded.com/market-story/northvolt-s-strategic-move-a-game-changer-in-the-ev-battery-industry",
+  },
+  {
+    title: "Enifer's Mycoprotein Venture: Sustainable Protein Revolution ",
+    description:
+      "Finnish startup Enifer lands €12M grant for mycoprotein production, offering a sustainable alternative for food, pet care, and aquaculture, propelled by the EU's circular economy goals.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-protein-production-enifer-s-mycoprotein-venture",
+  },
+  {
+    title: "Stellar Health's Value-Based Care Expansion with UNC",
+    description:
+      "Stellar Health expands its partnership with UNC Health Alliance, redefining value-based care with innovative payment models and technology-driven healthcare solutions.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-value-based-care-stellar-health-and-unc-health-alliance-s-strategic-expansion",
+  },
+  {
+    title: "PlantArcBio's Herbicide Resistant Soybean: USDA Approval",
+    description:
+      "PlantArcBio secures USDA approval for its genetically modified soybean, leveraging innovative genetic technology for sustainable agriculture and weed management.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-agriculture-plantarcbio-s-breakthrough-in-sustainable-crop-management",
+  },
+  {
+    title: "Perfect Day's Leadership Shift and $90M Funding",
+    description:
+      "Perfect Day, a leader in animal-free dairy, secures $90M in pre-series E funding and announces a strategic leadership transition to scale production and profitability.",
+    link: "https://www.marketunwinded.com/market-story/perfect-day-precision-fermentation-pioneers-scale-new-heights",
+  },
+  {
+    title: "Elevance Health Acquires Paragon: Expanding Health Services",
+    description:
+      "Elevance Health's acquisition of Paragon Healthcare broadens its healthcare services, enhancing access to specialty medications and infusion therapies for chronic illnesses.",
+    link: "https://www.marketunwinded.com/market-story/elevance-health-s-acquisition-of-paragon-healthcare-a-strategic-move-in-health-services",
+  },
+  {
+    title: "WiTricity and ICON EV: Wireless Charging for LSVs",
+    description:
+      "WiTricity and ICON EV launch wireless charging for Low-Speed Vehicles (LSVs), introducing an industry-first feature to enhance convenience and user experience.",
+    link: "https://www.marketunwinded.com/market-story/witricity-icon-ev-electrifying-convenience-wireless-charging-revolutionizes-low-speed-vehicles",
+  },
+  {
+    title: "Valens Semiconductor and Intel Foundry Services' Alliance",
+    description:
+      "Valens Semiconductor partners with Intel Foundry Services to manufacture advanced MIPI A-PHY chipsets, accelerating automotive safety and connectivity solutions.",
+    link: "https://www.marketunwinded.com/market-story/accelerating-automotive-safety-valens-semiconductor-and-intel-foundry-services-forge-a-transformati",
+  },
+  {
+    title: "TheraVet's Expansion in Italy: Advancing Pet Healthcare",
+    description:
+      "TheraVet expands into Italy's pet healthcare market with BIOCERA-VET®, a bone substitute line, through a partnership with Alcyon Italia, tapping into Europe's fourth-largest pet market.",
+    link: "https://www.marketunwinded.com/market-story/theravet-s-strategic-expansion-into-italy-a-pioneering-leap-in-pet-healthcare",
+  },
+  {
+    title: "Venus Aerospace's Hypersonic Milestone with NASA",
+    description:
+      "Venus Aerospace, in partnership with NASA, achieves a major breakthrough in hypersonic technology, promising revolutionary advancements in aerospace propulsion and space exploration.",
+    link: "https://www.marketunwinded.com/market-story/venus-aerospace-s-breakthrough-in-hypersonic-technology-a-game-changer-in-aerospace-innovation",
+  },
+  {
+    title: "Connected Farms NZ Launches CommsXtend",
+    description:
+      "Connected Farms NZ introduces CommsXtend, a groundbreaking Wi-Fi hotspot for New Zealand's rural areas, enhancing connectivity and digital agriculture capabilities in farmlands.",
+    link: "https://www.marketunwinded.com/market-story/connected-farms-nz-revolutionizes-rural-connectivity-with-commsxtend",
+  },
+  {
+    title: "Stellantis Ventures Backs Tiamat's Sodium-Ion Tech",
+    description:
+      "Stellantis Ventures invests in Tiamat to pioneer sodium-ion battery technology, advancing Stellantis' sustainable mobility mission and carbon neutrality goals.",
+    link: "https://www.marketunwinded.com/market-story/stellantis-ventures-invests-in-tiamat-pioneering-sodium-ion-battery-technology",
+  },
+  {
+    title: "Renesas Acquires Transphorm: Powering the Future",
+    description:
+      "Renesas Electronics acquires Transphorm for $339 million, gaining access to advanced GaN power semiconductors, bolstering its presence in EVs and high-efficiency power systems.",
+    link: "https://www.marketunwinded.com/market-story/revolutionizing-power-semiconductors-renesas-strategic-leap-with-transphorm-acquisition",
+  },
+];
