7afd964dcffb0217a952b9a4502c4b3969ae39aa
nileshaggarwal <aggarwalnilesh6@gmail.com>
2024-01-30 23:34:57 +0530

    Added search bar

--- app/chat/components/ChatPanel/QuestionsList.jsx
+++ app/chat/components/ChatPanel/QuestionsList.jsx
@@ -1,12 +1,15 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../store";
 import getGetCurrentThread from "./actions/getCurrentThread";
 import { switchActiveThread } from "./reducers";
+import Fuse from "fuse.js";
 
 const QuestionsList = () => {
   const { conversations } = useAppSelector((state) => state.chatPanel);
   const activeThread = useAppSelector((state) => state.chatPanel.activeThread);
   const dispatch = useAppDispatch();
+  const [filterQuery, setFilterQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   function handleThreadClick(conversationId) {
     dispatch(switchActiveThread(conversationId));
@@ -29,12 +32,35 @@ const QuestionsList = () => {
     }
   }
 
+  const fuse = new Fuse(conversations, {
+    keys: ["title", "thread.answer", "thread.query"],
+    includeScore: true,
+    threshold: 0.3,
+  });
+
+  useEffect(() => {
+    if (filterQuery.trim() === "") {
+      setSearchResults(conversations);
+    } else {
+      const results = fuse.search(filterQuery);
+      setSearchResults(results.map((result) => result.item));
+    }
+  }, [filterQuery, conversations]);
+
   console.log(activeThread);
 
   return (
     <div className="flex flex-col space-y-2 grow h-full noscrollbarstyle overflow-x-hidden  overflow-y-scroll py-5">
-      {conversations &&
-        conversations?.map((convo, index) => {
+      {conversations?.length > 0 && (
+        <input
+          placeholder="Quwey conversations"
+          className="border-b bg-transparent mb-2 w-11/12 mx-auto outline-none focus:border-purple-400 py-1 text-lg px-1"
+          value={filterQuery}
+          onChange={(e) => setFilterQuery(e.target.value)}
+        />
+      )}
+      {searchResults &&
+        searchResults?.map((convo, index) => {
           return (
             <div
               key={index}
